2f9416cd4f191724ebbe290af9797b137f327502
zxss702 <zxs20060621@icloud.com>
2023-12-29 18:52:01 +0800

    Update GlazedEnvironmentView.swift

--- Sources/Glazed/GlazedEnvironmentView.swift
+++ Sources/Glazed/GlazedEnvironmentView.swift
@@ -35,3 +35,55 @@ extension View{
             }
     }
 }
+
+extension DispatchQueue {
+    func async(_ name: String = "async", Action: @escaping () throws -> Void) {
+        self.async(execute: DispatchWorkItem(block: {
+            do {
+                try Action()
+            } catch {
+                print(error)
+            }
+//            print(name)
+        }))
+    }
+    func async(_ wate: Double = 0, name: String = "async", Action: @escaping () throws -> Void) {
+        self.asyncAfter(deadline: .now() + wate) {
+            do {
+                try Action()
+            } catch {
+                print(error)
+            }
+//            print(name)
+        }
+    }
+}
+
+extension View {
+    func ifMode(@ViewBuilder ifAction: (AnyView) -> some View) -> some View {
+        ifAction(AnyView(self))
+    }
+    
+    @ViewBuilder
+    func provided(_ Bool: Bool, _ ifAction: (AnyView) -> some View, else elseAction: (AnyView) -> some View = { AnyView in return AnyView}) -> some View {
+        if Bool {
+            ifAction(AnyView(self))
+        } else {
+            elseAction(AnyView(self))
+        }
+    }
+    
+    func shadow(Ofset: CGPoint = .zero) -> some View {
+        self
+            .shadow(radius: 0.3)
+            .shadow(color: Color(.sRGBLinear, white: 0, opacity: 0.2), radius: 35, x: Ofset.x, y: Ofset.y)
+    }
+    func shadow(size: CGFloat, Ofset: CGPoint = .zero) -> some View {
+        self
+            .shadow(radius: size, x: Ofset.x, y: Ofset.y)
+    }
+    func shadow(color: Color, size: CGFloat, Ofset: CGPoint = .zero) -> some View {
+        self
+            .shadow(color: color, radius: size, x: Ofset.x, y: Ofset.y)
+    }
+}
