3e96bb633cb14a280bd70a247378df802991bf84
张旭晟 知阳 <zhangxusheng@huaxiadaqian.com>
2024-09-27 16:15:26 +0800

    修复一些奇怪的问题

--- Sources/Glazed/UIShaowd.swift
+++ Sources/Glazed/UIShaowd.swift
@@ -6,81 +6,35 @@
 //
 
 import SwiftUI
+//
+//  UIShaowd.swift
+//  书笺
+//
+//  Created by 张旭晟 on 2023/12/18.
+//
 
-#if os(macOS)
-public struct UIShaowd: NSViewControllerRepresentable {
-    public typealias NSViewControllerType = UIShaowdViewController
+import SwiftUI
+
+struct UIShaowd: UIViewControllerRepresentable {
+    typealias UIViewControllerType = UIShaowdViewController
     
     let radius: CGFloat
     let cornerRaduiu: CGFloat
-    var color: Color = Color(.sRGBLinear, white: 0, opacity: 0.2)
+    var color: Color? = nil
     
-    public func makeNSViewController(context: Context) -> UIShaowdViewController {
+    @Environment(\.colorScheme) var colorScheme
+    
+    func makeUIViewController(context: Context) -> UIShaowdViewController {
         return UIShaowdViewController(RootView: self)
     }
     
-    public func updateNSViewController(_ nsViewController: UIShaowdViewController, context: Context) {
-        nsViewController.RootView = self
-        nsViewController.SetShadow()
-    }
-    
-    public init(radius: CGFloat, cornerRaduiu: CGFloat, color: Color = Color(.sRGBLinear, white: 0, opacity: 0.2)) {
-        self.radius = radius
-        self.cornerRaduiu = cornerRaduiu
-        self.color = color
-    }
-}
-
-public class UIShaowdViewController: NSViewController {
-    var RootView: UIShaowd
-    
-    init(RootView: UIShaowd) {
-        self.RootView = RootView
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    public override func viewWillAppear() {
-        SetShadow()
-    }
-    func SetShadow() {
-        view.layer?.shadowColor = RootView.color.cgColor
-        view.layer?.shadowOffset = CGSize(width: 0,height: 0)
-        view.layer?.shadowRadius = RootView.radius
-        view.layer?.shadowOpacity = 1
-        view.layer?.shadowPath = CGPath(roundedRect: view.bounds, cornerWidth: RootView.cornerRaduiu, cornerHeight: RootView.cornerRaduiu, transform: nil)
-    }
-    public override func viewDidLayout() {
-        SetShadow()
-    }
-}
-#else
-public struct UIShaowd: UIViewControllerRepresentable {
-    public typealias UIViewControllerType = UIShaowdViewController
-    
-    let radius: CGFloat
-    let cornerRaduiu: CGFloat
-    var color: Color = Color(.sRGBLinear, white: 0, opacity: 0.2)
-    
-    public func makeUIViewController(context: Context) -> UIShaowdViewController {
-        return UIShaowdViewController(RootView: self)
-    }
-    
-    public func updateUIViewController(_ uiViewController: UIShaowdViewController, context: Context) {
+    func updateUIViewController(_ uiViewController: UIShaowdViewController, context: Context) {
         uiViewController.RootView = self
         uiViewController.SetShadow()
     }
-    
-    public init(radius: CGFloat, cornerRaduiu: CGFloat, color: Color = Color(.sRGBLinear, white: 0, opacity: 0.2)) {
-        self.radius = radius
-        self.cornerRaduiu = cornerRaduiu
-        self.color = color
-    }
 }
 
-public class UIShaowdViewController: UIViewController {
+class UIShaowdViewController: UIViewController {
     var RootView: UIShaowd
     
     init(RootView: UIShaowd) {
@@ -92,22 +46,84 @@ public class UIShaowdViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-    public override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
         SetShadow()
     }
     
     func SetShadow() {
-        view.layer.shadowColor = RootView.color.cgColor
-        view.layer.shadowOffset = CGSize(width: 0,height: 0)
+        switch RootView.colorScheme {
+        case .dark:
+            view.layer.shadowColor = Color(.sRGBLinear, white: 0, opacity: 0.2).cgColor
+        case .light:
+            view.layer.shadowColor = Color(.sRGBLinear, white: 1, opacity: 0.2).cgColor
+        @unknown default:
+            view.layer.shadowColor = Color(.sRGBLinear, white: 0, opacity: 0.2).cgColor
+        }
+        view.layer.shadowOffset = CGSize(width: 0, height: 0)
         view.layer.shadowRadius = RootView.radius
         view.layer.shadowOpacity = 1
         view.layer.shadowPath = UIBezierPath(roundedRect: view.bounds, cornerRadius: RootView.cornerRaduiu).cgPath
     }
     
-    public override func viewDidLayoutSubviews() {
+    override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         SetShadow()
     }
 }
-#endif
+struct UIPathShaowd: UIViewControllerRepresentable {
+    typealias UIViewControllerType = UIPathShaowdViewController
+    
+    let radius: CGFloat
+    let path: CGPath
+    var color: Color? = nil
+    
+    @Environment(\.colorScheme) var colorScheme
+    
+    func makeUIViewController(context: Context) -> UIPathShaowdViewController {
+        return UIPathShaowdViewController(RootView: self)
+    }
+    
+    func updateUIViewController(_ uiViewController: UIPathShaowdViewController, context: Context) {
+        uiViewController.RootView = self
+        uiViewController.SetShadow()
+    }
+}
+
+class UIPathShaowdViewController: UIViewController {
+    var RootView: UIPathShaowd
+    
+    init(RootView: UIPathShaowd) {
+        self.RootView = RootView
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        SetShadow()
+    }
+    
+    func SetShadow() {
+        switch RootView.colorScheme {
+        case .dark:
+            view.layer.shadowColor = Color(.sRGBLinear, white: 0, opacity: 0.2).cgColor
+        case .light:
+            view.layer.shadowColor = Color(.sRGBLinear, white: 1, opacity: 0.2).cgColor
+        @unknown default:
+            view.layer.shadowColor = Color(.sRGBLinear, white: 0, opacity: 0.2).cgColor
+        }
+        view.layer.shadowOffset = CGSize(width: 0,height: 0)
+        view.layer.shadowRadius = RootView.radius
+        view.layer.shadowOpacity = 1
+        view.layer.shadowPath = RootView.path
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        SetShadow()
+    }
+}
